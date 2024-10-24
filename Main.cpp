271fb2bb73da1fb02c403d9db16dac1588d4a69e
Snuskasaurus <julien.rogel@gmail.com>
2024-10-24 02:03:16 +0200

    Window & Message Loop

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,33 @@
 // https://learn.microsoft.com/fr-fr/windows/win32/api/winbase/nf-winbase-winmain
 // https://learn.microsoft.com/en-us/windows/win32/winmsg/window-class-styles
 
-
 #include <Windows.h>
+
+const wchar_t* G_GameClassName = L"JuProject";
+const wchar_t* G_WindowName = L"JuProject";
+constexpr DWORD G_WindowDword = WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU;
+constexpr int G_WindowPositionX = 640;
+constexpr int G_WindowPositionY = 360;
+constexpr int G_WindowSizeX = 1280;
+constexpr int G_WindowSizeY = 720;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(88);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC; // Allocates a unique device context for each window in the class. Used to support multiples windows
-	wc.lpfnWndProc = DefWindowProc;
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -18,15 +36,29 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLin
 	wc.hbrBackground = nullptr;
 	wc.lpszMenuName = nullptr;
 	wc.hIconSm = nullptr;
-
-	const auto GameWindowName = L"Julien's Project";
-	wc.lpszClassName = GameWindowName;
-
+	wc.lpszClassName = G_GameClassName;
 	RegisterClassEx(&wc);
-
-	while (true)
+	
+	const HWND hwnd = CreateWindowEx(0, G_GameClassName, G_WindowName,
+		G_WindowDword,G_WindowPositionX, G_WindowPositionY, G_WindowSizeX,
+		G_WindowSizeY, nullptr, nullptr, hInstance, nullptr);
+	
+	ShowWindow(hwnd, SW_SHOW);
+	
+	MSG msg;
+	BOOL gResult;
+	do
 	{
-
+		gResult = GetMessage(&msg, nullptr, 0, 0);
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	} while (gResult > 0);
+	if (gResult == -1)
+	{
+		return -1;
 	}
-	return 0;
+	else
+	{
+		return (int)msg.wParam;
+	}
 }
