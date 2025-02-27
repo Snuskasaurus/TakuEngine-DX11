28692760d4f9a2186df1bde412db1f01e46d6b9b
Julien Rogel <julien.rogel@gmail.com>
2025-02-27 13:50:16 +0100

    changed the defautl map back to the grid

--- Source/Engine/World.cpp
+++ Source/Engine/World.cpp
@@ -19,7 +19,7 @@ void MWorld::OnInit()
     FreeLookCamera.SetPosition(TVector3f(0.0f, -80.0f, 30.0f));
     FreeLookCamera.SetRotation(TRotator(0.0f, -0.5f, 0.0f));
     
-    CGameScene::ChangeGameScene(EGameSceneType::TestPBRScene);
+    CGameScene::ChangeGameScene(EGameSceneType::GridScene);
 }
 ///---------------------------------------------------------------------------------------------------------------------
 void MWorld::OnUpdate(const float& _dt)
